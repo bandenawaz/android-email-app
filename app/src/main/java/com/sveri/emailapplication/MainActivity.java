2756e82206265bade3469ba67138855f65e71a49
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-17 11:08:38 +0530

    Final Commit

--- app/src/main/java/com/sveri/emailapplication/MainActivity.java
+++ app/src/main/java/com/sveri/emailapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.sveri.emailapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +31,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                //Lets get the text from the text boxes and store in a string variable
+                String emailTo = etEmail.getText().toString().trim();
+                String subj = etSubj.getText().toString().trim();
+                String msg = etBody.getText().toString().trim();
 
+                //lets define an intent
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+                //lets pass the data to email app
+                emailIntent.putExtra(Intent.EXTRA_EMAIL,new String[]{ emailTo});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, subj);
+                emailIntent.putExtra(Intent.EXTRA_TEXT, msg);
+
+                //lets set the type of intent
+                emailIntent.setType("messge/rfc822");
+
+                //lets launch the activity
+                startActivity(Intent.createChooser(emailIntent, "Choose email client :"));
+
+                etEmail.setText(null);
+                etBody.setText(null);
+                etSubj.setText(null);
             }
         });
     }
